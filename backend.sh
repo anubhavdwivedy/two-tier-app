c252a742b0562b41f97a684336f824cdf3a60bdf
anubhavdwivedy <anubhavdwivedy@gmail.com>
2024-08-06 14:53:47 +0530

    Create backend.sh

---
+++ backend.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#uninstall and clean old docker data
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras -y
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
+
+# Update package list
+sudo apt-get update -y
+
+# Add Docker’s official GPG key
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.as
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the package list again
+sudo apt-get update -y
+
+# Install Docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Add the current user to the Docker group
+
+# start and enable Docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Print Docker version to verify installation
+docker --version
+
+# pull the application from dockerhub
+docker pull anubhavdwivedy/flaskapp:latest
+docker pull mysql:5.7
